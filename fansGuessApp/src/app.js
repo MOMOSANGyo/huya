f3d16796875895e8b88b465ee11e1cf44e7d3edb
ll491119940 <491119940@qq.com>
2019-07-22 10:07:56 +0800

    添加封装接口函数、修改主播端路由

--- fansGuessApp/src/app.js
+++ fansGuessApp/src/app.js
@@ -7,7 +7,6 @@ import LoadingView from './anchor/components/A2Loading'
 import PrepareView from './anchor/components/A3Prepare'
 import PlayView from './anchor/components/A4Play'
 import 'antd/dist/antd.css'
-
 import './user/assets/scss/common.scss'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
--- fansGuessApp/src/streamerApp.js
+++ fansGuessApp/src/streamerApp.js
@@ -1,17 +1,23 @@
-
 import React from 'react'
 import { render } from 'react-dom'
 import classnames from 'classnames'
-import Avatar from './anchor/components/Avatar'
-import './anchor/assets/scss/common.scss'
-
-let isDemo = true;
-
+import MainView from './anchor/components/A1Main'
+import LoadingView from './anchor/components/A2Loading'
+import PrepareView from './anchor/components/A3Prepare'
+import PlayView from './anchor/components/A4Play'
+import 'antd/dist/antd.css'
+import './user/assets/scss/common.scss'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+console.log('=====22222222====')
 render(
-  <div className={classnames({'demo': isDemo})}>
-  	<Avatar/>
-  	<h1>Welcome to Your React.js Ext App</h1>
-  	<h2>Streamer side</h2>
-  </div>,
+  (
+    <Router>
+        <Redirect from="/" to="main" />
+        <Route path="/main" component={MainView} />
+        <Route path="/loading" component={LoadingView} />
+        <Route path="/prepare" component={PrepareView} />
+        <Route path="/play" component={PlayView} />
+    </Router>
+  ),
   document.getElementById('aroot')
 )
